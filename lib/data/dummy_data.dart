f2cdc9ae2525a4c45232c6c465b21354bbaf7885
fidahussainqazafi <fidahussainqazafi60@gmail.com>
2023-12-04 11:52:48 +0500

    category model added

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/model/category.dart';
+
+
+  const availableCategories = [
+    Category(id: 'c1',
+        title: 'Italian',
+      color: Colors.purple,
+
+    ),
+    Category(id: 'c2',
+      title: 'Quick and easy',
+      color: Colors.red,
+
+    ),
+
+    Category(id: 'c3',
+      title: 'Hambergun',
+      color: Colors.orange,
+
+    ),
+
+    Category(id: 'c4',
+      title: 'German',
+      color: Colors.amber,
+
+    ),
+    Category(id: 'c5',
+      title: 'Light and Lovely',
+      color: Colors.blue,
+
+    ),
+
+    Category(id: 'c6',
+      title: 'Exotic',
+      color: Colors.green,
+
+    ),
+  ];
